cbbb9b58cafb4456d72b7d7063f79c7abcbbf42e
Alec Jones <alecaej2002@gmail.com>
2017-02-24 20:16:01 -0800

    stuff

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -380,7 +380,7 @@ def self.sendGroupConfirmMessage(recipient, possibleClasses, charge)
       return "don't be serious; be silly ;-)"
     elsif textArray.include?("why?") || textArray.include?("why")
       return "i'm not good at giving answers :-|"
-    elsif textArray.include?("stupid")
+    elsif textArray.include?("stupid") || textArray.include?("dumb")
       return "woah! don't be rude :-("
     elsif textArray.include?("awesome")
       return "way to be postive...it's impossible for me to be happy because i'm a bot...but now i feel sad thinking about it"
